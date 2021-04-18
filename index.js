bf77ba4222c52175fbe71046285d62e13ab9f5ee
TepanD <stefanwork12@gmail.com>
2021-04-18 19:24:36 +0700

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("body", function (req, res) {
@@ -111,7 +112,7 @@ app.get("/api/info", (requests, response) => {
   `);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
